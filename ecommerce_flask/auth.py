e38cd3accd13c2449ce32dc187a89ca70f3ef1c6
obuyajr <felix37brock@gmail.com>
2023-04-16 00:36:12 +0300

    signup

--- ecommerce_flask/auth.py
+++ ecommerce_flask/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET','POST'])
 def login():
     return render_template("login.html")
 
@@ -10,7 +10,12 @@ def login():
 def logout():
     return "<p>LOGOUT<p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET','POST'])
 def sign_up():
+      if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
     return render_template("sign_up.html")
 
