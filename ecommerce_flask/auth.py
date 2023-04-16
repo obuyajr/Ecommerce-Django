a2dcfe07b3c66c6683305a7767f45879523cf006
obuyajr <felix37brock@gmail.com>
2023-04-16 11:11:49 +0300

    flash message

--- ecommerce_flask/auth.py
+++ ecommerce_flask/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods = ['GET','POST'])
 def login():
+    
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -18,24 +19,16 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if user:
-                flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+        if len(email) > 4:
+            flash('Email must be greater than 4 characters',category = 'error')
+        elif len(firstName)<2:
+             flash('First name must be greater than 4 characters',category = 'error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+             flash('passwordsa dont match',category = 'error')
+        elif len(password1)< 7:
+             flash('password must be atleast 7 characters',category = 'error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
-
-    return render_template("sign_up.html")
+             flash('Account created ',category = 'success')
+            
+        return render_template("sign_up.html")
 
