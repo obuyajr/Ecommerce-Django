c4b599bad47d7899e0b641bc03d8a15024d6f04f
obuyajr <66009556+obuyajr@users.noreply.github.com>
2023-04-17 22:06:45 +0300

    Update models.py
    db complete

--- ecommerce_flask/models.py
+++ ecommerce_flask/models.py
@@ -9,3 +9,9 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
