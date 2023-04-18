c2b99aeeaa3710226532c7b4388f0615cd1da793
obuyajr <66009556+obuyajr@users.noreply.github.com>
2023-04-19 00:00:15 +0300

    Update auth.py

--- ecommerce_flask/auth.py
+++ ecommerce_flask/auth.py
@@ -1,29 +1,49 @@
-from flask import Blueprint, render_template, request, flash, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
-auth = Blueprint('auth',__name__)
 
-@auth.route('/login', methods = ['GET','POST'])
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>LOGOUT<p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods = ['GET','POST'])
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-      if request.method == 'POST':
+    if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
-           user = User.query.filter_by(email=email).first()
+
+        user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
         elif len(email) < 4:
@@ -42,6 +62,5 @@ def sign_up():
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-            
-        return render_template("sign_up.html")
 
+    return render_template("sign_up.html", user=current_user)
